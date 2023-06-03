f4e4e89d11b581a00ec5d0cd816ef08ea232de98
facupaez <facu.pz10@gmail.com>
2023-06-02 22:49:11 -0300

    building api rest project

---
+++ Concepts/spring-api-rest/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    // CRUD sobre la entidad Book en base de datos
+
+    // buscar todos los libros
+
+    // buscar libro por id
+
+    // crear un nuevo libro
+
+    // modificar un libro
+
+    // eliminar un libro
+}
---
+++ Concepts/spring-api-rest/src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                  <!doctype html>
+                   <html lang="en">
+                     <head>
+                       <meta charset="utf-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1">
+                       <title>Bootstrap demo</title>
+                       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+                     </head>
+                     <body>
+                       <h1>Hello, world from Spring Boot with Bootstrap!</h1>
+                       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+                     </body>
+                   </html>
+                """;
+    }
+}
