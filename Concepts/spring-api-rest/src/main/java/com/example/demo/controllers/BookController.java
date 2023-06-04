45cda868f1ca1281a6cafe6298df739aded0dbab
facupaez <facu.pz10@gmail.com>
2023-06-04 00:21:39 -0300

    api rest CRUD: find book by id

--- Concepts/spring-api-rest/src/main/java/com/example/demo/controllers/BookController.java
+++ Concepts/spring-api-rest/src/main/java/com/example/demo/controllers/BookController.java
@@ -2,10 +2,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.BookEntity;
 import com.example.demo.repositories.BookRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -21,11 +24,17 @@ public class BookController {
 
     // buscar todos los libros
     @GetMapping("/books")
-    public List<BookEntity> findAll(){
+    public List<BookEntity> findAll() {
         return bookRepository.findAll();
     }
 
     // buscar libro por id
+    @GetMapping("/books/{id}")
+    // usando ResponseEntity ayudamos a la api a entender las respuetas que debe emitir al tener errores
+    public ResponseEntity<BookEntity> findById(@PathVariable Long id) {
+        Optional<BookEntity> bookOpt = bookRepository.findById(id);
+        return bookOpt.isPresent() ? ResponseEntity.ok(bookOpt.get()) : ResponseEntity.notFound().build();
+    }
 
     // crear un nuevo libro
 
