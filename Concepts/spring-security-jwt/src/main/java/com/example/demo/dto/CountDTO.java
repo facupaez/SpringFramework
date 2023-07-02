f5116053c86ff67f58133e78d38d5ca50ec93090
facupaez <facu.pz10@gmail.com>
2023-07-02 01:15:57 -0300

    spring security jwt

---
+++ Concepts/spring-security-jwt/src/main/java/com/example/demo/dto/CountDTO.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto;
+
+public class CountDTO {
+
+    private Long count;
+    private String message;
+
+    public CountDTO() {
+    }
+
+    public CountDTO(Long count) {
+        this.count = count;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+
+}
